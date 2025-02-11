9ed7745c6569a519a55e8067966ff563169591bf
Steven Fischer <swcfischer@gmail.com>
2025-02-11 17:27:48 -0600

    changes

--- models/Book.js
+++ models/Book.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
     },
     image: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(1024),
       allowNull: true,
       defaultValue: "",
     },
