b95978a34cc116e70f6d62e14e75ca7885e50fa8
Steven Fischer <swcfischer@gmail.com>
2025-02-13 04:15:15 -0600

    changes

--- models/index.js
+++ models/index.js
@@ -36,9 +36,9 @@ ReadingSession.belongsTo(Book, { foreignKey: "bookId" });
 
 Book.hasMany(ReadingSession, { foreignKey: "bookId" });
 
-User.sync({ force: false })
-  .then(() => Book.sync({ force: false }))
-  .then(() => ReadingSession.sync({ force: false }))
+User.sync({ force: true })
+  .then(() => Book.sync({ force: true }))
+  .then(() => ReadingSession.sync({ force: true }))
   .catch((err) => console.error(err));
 
 module.exports = {
