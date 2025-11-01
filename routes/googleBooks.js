11acccd317f55a6bd1de85ee55203eba472d5440
Steven Fischer <swcfischer@gmail.com>
2025-11-01 00:41:25 -0500

    changes

--- routes/googleBooks.js
+++ routes/googleBooks.js
@@ -25,7 +25,7 @@ router.get("/search/:userUuid", isAuthorized, async (req, res) => {
 
     // Make request to Google Books API with valid token
     const response = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes?${queryParams}&orderBy=relevance`,
+      `https://www.googleapis.com/books/v1/volumes?${queryParams}&orderBy=relevance&filter=full&printType=books`,
       {
         headers: {
           Authorization: `Bearer ${accessToken}`,
